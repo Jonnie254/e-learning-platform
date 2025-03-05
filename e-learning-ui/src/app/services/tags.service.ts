4423003b04fdfe62fd29b8982fe1571bebe8de6e
jonnie254 <johnkamanu254@proton.me>
2025-03-05 03:11:46 +0300

    add tag-categories service

---
+++ e-learning-ui/src/app/services/tags.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from './auth-service.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TagsService {
+
+  constructor(
+    private httpClient: HttpClient,
+    private authService: AuthService
+  ) { }
+}
