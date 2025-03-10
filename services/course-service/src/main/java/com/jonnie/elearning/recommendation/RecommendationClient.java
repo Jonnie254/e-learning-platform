21fae149f60f8659a0fc5a6695feb37c0f10f8f7
jonnie254 <johnkamanu254@proton.me>
2025-03-11 02:01:31 +0300

    create the personalized recommendation

--- services/course-service/src/main/java/com/jonnie/elearning/recommendation/RecommendationClient.java
+++ services/course-service/src/main/java/com/jonnie/elearning/recommendation/RecommendationClient.java
@@ -1,6 +1,5 @@
 package com.jonnie.elearning.recommendation;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +8,7 @@ import java.util.Map;
 
 @FeignClient(
         name = "recommendationClient",
-        url = "http://localhost:5000")
+        url = "${application.config.recommendation-url}")
 public interface RecommendationClient {
     @GetMapping("/recommend")
     Map<String, Object> getRecommendations(@RequestParam("user_id") String userId);
